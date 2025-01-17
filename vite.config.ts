99953cbc1c5003d8be8d4a2fadf9083e5ab0fe98
Alexis Thierry <alexist103@gmail.com>
2025-01-17 19:32:10 +0200

    [feature] update stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
 	base: "./",
+	plugins: [react()],
 	preview: {
 		port: 8080,
 		strictPort: true,
@@ -13,10 +15,4 @@ export default defineConfig({
 		host: true,
 		origin: "http://0.0.0.0:8080",
 	},
-  resolve: {
-    alias: {
-        react: "react",
-        "react-dom": "react-dom",
-    },
-},
 });
